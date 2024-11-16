bf9abe19d4b2ad8b9f5c61541703cdf7cba82b0a
Phpcesare <pedrohenriqueej8@gmail.com>
2024-11-16 10:52:59 -0300

    v1.0

---
+++ Agrinho_2024/Pedrohenriquepavanicesare/script.js
@@ -0,0 +1,9 @@
+window.transicaoDePagina = function(href){
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function(){
+        window.location.href = href
+    }, 500)
+}
+document.addEventListener('DOMContentLoaded', function(event){
+    document.querySelector('body').style.opacity = 1
+})
