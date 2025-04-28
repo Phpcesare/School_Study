5837f8c266357ceab5588f7ebe08caca9aef2259
Pedro Henrique Pavani Cesare <161896313+Phpcesare@users.noreply.github.com>
2025-04-28 10:59:23 -0300

    update

--- colegio_adolpho/programacao_back-end/atividades/atividade 3/script.js
+++ colegio_adolpho/programacao_back-end/atividades/atividade 3/script.js
@@ -1,22 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
-
-    let tamanhoAtualFonte = 1;
-
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
-    })
-
 })
-document.getElementById('botao-acessibilidade')
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+let tamanhoAtualFonte = 1;
+aumentaFonteBotao.addEventListener('click', function () {
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+})
+diminuiFonteBotao.addEventListener('click', function () {
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`  
+    document.querySelector('button#botao-acessibilidade').addEventListener('click', function() {
+        acessibilidade = document.getElementById('opcoes-acessibilidade').style.display = 'block';
+})
+})
